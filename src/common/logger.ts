8805b68477e587efe473a996c4c58a9beb0839dd
Carlos <casantosmu@gmail.com>
2023-01-15 04:05:12 +0100

    Added logger and loggerHttp common

---
+++ src/common/logger.ts
@@ -0,0 +1,35 @@
+import pino from "pino";
+import pinoHttp from "pino-http";
+import { config } from "./config";
+
+type LogLevels = "debug" | "info" | "warn" | "error" | "fatal";
+
+const pinoLogger = pino({
+  level: config.isTest ? "silent" : config.logLevel,
+  messageKey: "message",
+  errorKey: "error",
+  formatters: {
+    level(_label, number) {
+      return { level: number };
+    },
+  },
+});
+
+export const loggerHttp = () =>
+  pinoHttp({
+    logger: pinoLogger,
+  });
+
+const logger =
+  (level: LogLevels) =>
+  (message: string, metadata?: Record<string, unknown> | Error) => {
+    pinoLogger[level](metadata, message);
+  };
+
+export default {
+  debug: logger("debug"),
+  error: logger("error"),
+  info: logger("info"),
+  warning: logger("warn"),
+  fatal: logger("fatal"),
+};
