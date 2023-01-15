a2b7c4036b4cfddec00a02d6597e5b139dd46632
Carlos <casantosmu@gmail.com>
2023-01-15 15:36:26 +0100

    Added handleError on error

---
+++ src/common/error/handleError.ts
@@ -0,0 +1,35 @@
+import CustomError from "./CustomError";
+import util from "util";
+import logger from "../logger";
+
+const normalizeError = (errorToHandle: unknown): CustomError => {
+  if (errorToHandle instanceof CustomError) {
+    return errorToHandle;
+  }
+
+  if (errorToHandle instanceof Error) {
+    const customError = new CustomError(
+      errorToHandle.name,
+      errorToHandle.message,
+      500,
+      { cause: errorToHandle }
+    );
+    customError.stack = errorToHandle.stack;
+    return customError;
+  }
+
+  const inputType = typeof errorToHandle;
+  return new CustomError(
+    "general-error",
+    `Error Handler received a none error instance with type - ${inputType}, value - ${util.inspect(
+      errorToHandle
+    )}`
+  );
+};
+
+const handleError = (errorToHandle: unknown) => {
+  const customError: CustomError = normalizeError(errorToHandle);
+  logger.error(customError.message, customError);
+};
+
+export default handleError;
