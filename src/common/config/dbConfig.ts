a868d45915dda441c0f4082df5f08feb59068003
Carlos <casantosmu@gmail.com>
2023-02-03 17:26:30 +0100

    Added BD configs

---
+++ src/common/config/dbConfig.ts
@@ -0,0 +1,7 @@
+import env from "./env";
+
+export const port = env.DB_PORT;
+export const host = env.DB_HOST;
+export const database = env.DB_NAME;
+export const user = env.DB_USER;
+export const password = env.DB_PASSWORD;
--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -1,1 +1,2 @@
 export { default as config } from "./config";
+export * as dbConfig from "./dbConfig";
